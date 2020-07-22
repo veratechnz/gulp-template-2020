19cd3ac6fc49851d38c9e5635679475ac5855eaf
veratech <jared@veratech.co.nz>
2020-07-22 18:28:40 +1200

    $1

---
+++ gulp-nasa-ui/js/custom-nasa.js
@@ -0,0 +1,24 @@
+// custom js file
+
+(function(){
+
+	function makeAjaxRequest () {
+		// GET request for remote image in node.js
+		axios({
+		  method: 'get',
+		  url: 'https://api.nasa.gov/planetary/apod?api_key=0a2OMkeeTLcnN7bPTfoDNdRcGHHTesp4JH88fl13'
+		}).then(function (response) {
+		   $('#generate').click(function(){
+		   	applyImage(response.data)
+		   });
+		});
+	}
+
+	makeAjaxRequest()
+
+	function applyImage (data) {
+		console.log(data)
+		$('#theImage').attr('src', data.hdurl);
+	}
+
+})(); // iffe ENDS
