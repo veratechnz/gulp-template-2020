47dfcf4c860fef0309f8cdc8ce248ac57059b1bd
veratech <jared@veratech.co.nz>
2020-07-23 21:48:55 +1200

    $1

--- js/custom.js
+++ js/custom.js
@@ -2,16 +2,27 @@
 
 (function(){
 
+	var app = new Vue({
+	  el: '#app',
+	  data: {
+	   	weather: false
+	  }
+	})
+
 	function makeAjaxRequest () {
 		// GET request for remote image in node.js
 		axios({
 		  method: 'get',
-		  url: 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minute&appid=66ce6f7e945db003aaa343f0bc010dc8'
+		  url: 'https://api.openweathermap.org/data/2.5/onecall?lat=-41.282626&lon=-174.773390&units=metric&exclude=hourly,minute&appid=66ce6f7e945db003aaa343f0bc010dc8'
 		}).then(function (response) {
+		 	app.weather = response.data.daily
 		 	console.log(response.data)
+		 	console.log(app.weather)
 		});
 	}
 
 	makeAjaxRequest()
+
+
 
 })(); // iffe ENDS
